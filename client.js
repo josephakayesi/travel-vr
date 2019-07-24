2b43ab5c578138e943bf5a4c288a2ea554006368
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-24 18:15:03 +0000

    complete project

--- client.js
+++ client.js
@@ -1,23 +1,46 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance, Location, Surface, Module } from 'react-360-web';
 
-function init(bundle, parent, options = {}) {
-  const r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
-    fullScreen: true,
-    ...options,
-  });
+class TitleChanger extends Module {
+    constructor() {
+        super('TitleChanger');
+    }
 
-  // Render your app content to the default cylinder surface
-  r360.renderToSurface(
-    r360.createRoot('Hello360', { /* initial props */ }),
-    r360.getDefaultSurface()
-  );
-
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+    changeTitle(title) {
+        document.title = 'Welcome to ' + title;
+    }
 }
 
-window.React360 = {init};
+function init(bundle, parent, options = {}) {
+    const r360 = new ReactInstance(bundle, parent, {
+        // Add custom options here
+        fullScreen: true,
+        nativeModules: [new TitleChanger()],
+        ...options
+    });
+
+    const myCylinderSurface = new Surface(
+        4680,
+        600,
+        Surface.SurfaceShape.Cylinder
+    );
+
+    // Render your app content to the default cylinder surface
+    r360.renderToSurface(
+        r360.createRoot('TravelVR', {
+            /* initial props */
+        }),
+        myCylinderSurface
+    );
+
+    const location = new Location([0, -1, -1]);
+
+    r360.renderToLocation(r360.createRoot('Earth'), location);
+
+    // Load the initial environment
+    r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+}
+
+window.React360 = { init };
